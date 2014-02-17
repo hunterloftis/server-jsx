816ce4e9d93b4c436e56961d8fb6440b14c24c6e
Hunter Loftis <hunter@recurly.com>
2014-02-17 14:41:42 -0800

    tests for failure cases

---
+++ test/fixtures/bad-template.jsx
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+      Route: { this.props.route }</h1>
+    );
+  }
+});
--- test/jsx-modules.test.js
+++ test/jsx-modules.test.js
@@ -11,4 +11,18 @@ describe('jsx-modules', function() {
   it('should return a ReactComponent instance', function() {
     assert.ok(react.isValidComponent(this.template));
   });
+  it("should error 'Cannot find' if the file doesn't exist", function() {
+    assert.throw(requireMissing, /Cannot find/);
+
+    function requireMissing() {
+      require('./missing.jsx');
+    }
+  });
+  it("should error 'Parse Error' if the jsx has errors", function() {
+    assert.throw(requireBad, /Parse Error/);
+
+    function requireBad() {
+      require('./fixtures/bad-template.jsx');
+    }
+  });
 });
